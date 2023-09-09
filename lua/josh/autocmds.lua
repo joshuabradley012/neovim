221b94a0f0f3e50ed52d1335fbee69414b70d96d
Joshua Bradley <joshuabradley012@gmail.com>
2023-09-09 15:32:18 -0700

    Update autocmds.lua

--- lua/josh/autocmds.lua
+++ lua/josh/autocmds.lua
@@ -1,20 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"*.arc"},
-  callback = function() set.filetype = "lisp" end,
-})
-
-autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"*.ftl"},
-  callback = function() set.filetype = "ftl" end,
-})
-
-autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"*.prisma"},
-  callback = function() set.filetype = "graphql" end,
-})
-
 autocmd({"BufWritePre"}, {
   pattern = {"*"},
   command = [[%s/\s+$//e]],
