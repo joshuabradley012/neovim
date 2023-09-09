be1a1e773761521af6cd671c4229e2034684ff15
Joshua Bradley <joshuabradley012@gmail.com>
2023-09-09 15:43:58 -0700

    Update plugins.lua

--- lua/josh/plugins.lua
+++ lua/josh/plugins.lua
@@ -13,19 +13,19 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
+    "nvim-telescope/telescope.nvim", tag = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      vim.cmd "TSUpdate"
+    end,
+  },
+  {
     "sainnhe/gruvbox-material",
     init = function()
       vim.cmd "colorscheme gruvbox-material"
     end,
   },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-      vim.cmd "TSUpdate"
-    end,
-  }
 })
