d49220697c1743f9e31f9256c0a04ac51cf83ed2
joshuabradley012 <joshuabradley012@gmail.com>
2024-07-08 21:05:36 -0700

    Fixed eslint

--- lua/josh/plugins.lua
+++ lua/josh/plugins.lua
@@ -27,9 +27,49 @@ require("lazy").setup({
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     config = function()
-      vim.cmd "TSUpdate"
-    end,
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = {
+          "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+          "jsdoc", "bash",
+        },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+        auto_install = true,
+
+        indent = {
+          enable = true
+        },
+
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = { "markdown" },
+        },
+      })
+
+      local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      treesitter_parser_config.templ = {
+        install_info = {
+          url = "https://github.com/vrischmann/tree-sitter-templ.git",
+          files = {"src/parser.c", "src/scanner.c"},
+          branch = "master",
+        },
+      }
+
+      vim.treesitter.language.register("templ", "templ")
+    end
   },
   {
     "github/copilot.vim",
@@ -47,72 +87,95 @@ require("lazy").setup({
   -- npm i -g vscode-langservers-extracted
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").eslint.setup({
-        on_attach = function(client, bufnr)
-          -- Enable ESLint fix on save
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-
-          -- Enable completion triggered by <c-x><c-o>
-          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-          -- Mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local bufopts = { noremap=true, silent=true, buffer=bufnr }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-          vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-          vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-          vim.keymap.set('n', '<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, bufopts)
-          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-          vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-        end,
-      })
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      require("null-ls").setup()
-    end,
-  },
-  {
-    "MunifTanjim/eslint.nvim",
     dependencies = {
-      "neovim/nvim-lspconfig",
-      "jose-elias-alvarez/null-ls.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
     },
+
     config = function()
-      require("eslint").setup({
-        bin = 'eslint_d', -- or `eslint_d`
-        code_actions = {
-          enable = true,
-          apply_on_save = {
-            enable = true,
-            types = { "directive", "problem", "suggestion", "layout" },
-          },
-          disable_rule_comment = {
-            enable = true,
-            location = "separate_line", -- or `same_line`
-          },
+      local cmp = require('cmp')
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
+
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "gopls",
+          "tsserver"
         },
-        diagnostics = {
-          enable = true,
-          report_unused_disable_directives = false,
-          run_on = "type", -- or `save`
+        handlers = {
+          function(server_name) -- default handler (optional)
+
+            require("lspconfig")[server_name].setup {
+              capabilities = capabilities
+            }
+          end,
+
+          ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  runtime = { version = "Lua 5.1" },
+                  diagnostics = {
+                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                  }
+                }
+              }
+            }
+          end,
+        }
+      })
+
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For luasnip users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
         },
       })
-    end,
+    end
   },
 })
--- lua/josh/remaps.lua
+++ lua/josh/remaps.lua
@@ -2,6 +2,7 @@ local opts = { noremap=true, silent=true }
 
 -- eslint
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>[', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', '<leader>]', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>s', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
